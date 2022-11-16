a574870d68d6052a060748c219169affb20c9ca5
Ieva <ieva@vugene.us>
2022-11-16 20:49:27 +0200

    Documentation of main functions to find methylation regions.

--- R/regions.R
+++ R/regions.R
@@ -60,20 +60,34 @@ splitChromosomes <- function(granges, maxSplit = 50000) {
 }
 
 #'
-#' TODO: explain all variables
-#' TODO: create an explanatory page with examples how to run the package
-#' TODO: check all calculations
-#' TODO: is there a way to optimize?
-#' TODO: what could go wrong? at what scenarios will the sigmoid fail?
-#' TODO: should this method be exported?
-#' TODO: can it be used with m-values, not beta values? why? explanations!
+#' Methylation Regions
+#'
+#' @description Calculates methylation regions for a subset of the dataset.
+#' When using this method, a single chromosome or its subset should be passed.
+#'
+#' @return Returns a named list. The names correspond to CpG names and the
+#' values are for that CpG assigned cluster. Note that not all CpGs that
+#' were in the original dataset will be returned. Some may not cluster at
+#' all, and thus will not be returned.
+#'
+#' @param beta Beta value (or m-value) matrix with probes in rows and
+#' samples in columns. Row names should correspond to names of the annotation
+#' (see \code{granges} argument).
+#' @param granges CpG annotations. It is a GRanges object containing sorted
+#' coordinates of all CpGs of interest. The names of this object should
+#' correspond to CpG names and match row names of the beta value matrix.
+#' @param thresholdDistance Threshold distance
+#' @param rateFactor Rate factor
+#' @param scale Should beta value matrix be scaled? (default: TRUE)
+#' @param minClusterSize Minimum number of CpGs per cluster, integer
+#' (default: 5).
+#' @param compressionRatio Compression ratio
+#' @param seed A seed for random number generator.
+#' @param verbose Should workflow messages be printed? (default: FALSE)
 #'
 #' @importFrom Rfast Dist
 #' @importFrom stats ecdf
-#' @importFrom stats hclust
-#'
-#'
-#' @export
+#' @importFrom stats hclust cutree
 #'
 methRegions <- function(
   beta,
@@ -88,8 +102,10 @@ methRegions <- function(
   set.seed(seed)
 
   if (!all(names(granges) %in% rownames(beta))) {
-    notPresent <- names(granges)[ !(names(granges) %in% rownames(beta)) ]
-    stop('Could not find probes ', paste(notPresent, collapse = ', '), ' in the beta value beta matrix')
+    warning('Some probes are missing in the beta value beta matrix. Will use only positions that are present in both annotation and beta value matrix.')
+
+    beta <- beta[ na.omit(match(names(granges), rownames(beta))), ]
+    granges <- granges[ names(granges) %in% rownames(beta) ]
   }
 
   beta <- beta[ names(granges), ]
@@ -97,7 +113,7 @@ methRegions <- function(
     stop('Methylation values should not contain missing values')
   }
 
-  # TODO: move out to wrapper
+  # TODO: discuss when this step should be performed
   if (verbose) message('[ Preparing methylation matrix ]')
   beta <- prepareBeta(beta, scale = scale, verbose = verbose)
 
@@ -107,7 +123,7 @@ methRegions <- function(
   colnames(distance) <- rownames(distance) <- names(granges)
 
   # TODO: this needs to be checked
-  w <- mapply(function(x) { x - start(granges) }, start(granges))
+  w <- base::mapply(function(x) { x - start(granges) }, start(granges))
   w <- abs(w)
   colnames(w) <- rownames(w) <- names(granges)
 
@@ -129,7 +145,7 @@ methRegions <- function(
   if (verbose) message('[ Cutting tree and calculating clusters ]')
   # TODO: test dynamic tree cut
   k <- max(floor(ncol(X) / compressionRatio), 1)
-  clusters <- cutree(hclustering, k = k)
+  clusters <- stats::cutree(hclustering, k = k)
   bad <- which(table(clusters) < minClusterSize)
   clusters[ clusters %in% bad ] <- 0
   names(clusters) <- names(granges)
@@ -139,19 +155,28 @@ methRegions <- function(
 }
 
 #'
-#' @export
+#' Sigmoid function
+#'
+#' @param x Matrix
+#' @param k k
 #'
 sigmoid <- function(x, k = 1) {
   1 / (1 + exp(-k * x))
 }
 
 #'
-#' @export
+#' Beta Values
+#'
+#' @description Beta value preprocessing.
+#'
+#' @param beta Beta value matrix.
+#' @param scale Should beta value matrix be scaled? (default: TRUE)
+#' @param verbose Should workflow messages be printed? (default: FALSE)
 #'
 prepareBeta <- function(beta, scale = TRUE, verbose = TRUE) {
   if (scale == TRUE) {
     if (verbose) message('[ Scaling methylation matrix ]')
-    beta <- beta %>% t %>% scale %>% t
+    beta <- beta %>% base::t() %>% base::scale() %>% base::t()
   } else {
     if (verbose) message('[ Using non-scaled methylation matrix ]')
   }
@@ -160,15 +185,35 @@ prepareBeta <- function(beta, scale = TRUE, verbose = TRUE) {
 }
 
 #'
-#' @importFrom tibble enframe
-#' @import foreach
-#' @import dplyr
-#' @import data.table
+#' Methylation Regions
 #'
+#' @inheritParams methRegions
+#' @inheritParams sortAnnotations
+#' @inheritParams splitChromosomes
+#' @param allowParallel Should parallel processing be used. If you wish
+#' to use multiple cores, register cores using \code{doParallel} package.
+#'
+#' @importFrom tibble enframe
+#' @importFrom foreach foreach %do% %dopar%
+#' @importFrom dplyr %>%
+#' @importFrom data.table .I as.data.table ':=' .N data.table setnames
 #'
 #' @export
 #'
-findMethRegions <- function(granges, beta, allowParallel = FALSE, verbose = FALSE, chromosomes = NULL, maxSplit = 50000, ...) {
+findMethRegions <- function(
+  beta,
+  granges,
+  chromosomes = NULL,
+  maxSplit = 50000,
+  thresholdDistance = 5e+07,
+  rateFactor = 10,
+  scale = TRUE,
+  minClusterSize = 5,
+  compressionRatio = 10,
+  seed = 23985034,
+  allowParallel = FALSE,
+  verbose = FALSE
+) {
   granges <- sortAnnotations(granges, verbose = verbose, chromosomes = chromosomes)
   granges <- splitChromosomes(granges, maxSplit = maxSplit)
   if (verbose) message('[ Dataset was divided into ', length(granges), ' splits ]')
@@ -177,27 +222,35 @@ findMethRegions <- function(granges, beta, allowParallel = FALSE, verbose = FALS
     if (verbose) message('[ Will try to parallelize using doParallel ]')
 
     clusters <- foreach::foreach(i = names(granges), .combine = rbind) %dopar% {
-      methRegions(beta, granges[[ i ]], verbose = verbose, ...) %>%
+      methRegions(beta, granges[[ i ]], verbose = verbose,
+                  thresholdDistance = thresholdDistance,
+                  rateFactor = rateFactor, scale = scale,
+                  minClusterSize = minClusterSize,
+                  compressionRatio = compressionRatio, seed = seed) %>%
         tibble::enframe(name = 'Name', 'Cluster') %>%
-        as.data.table %>%
+        data.table::as.data.table %>%
         .[ , Cluster := paste(i, Cluster, sep = '_') ]
     }
   } else {
     if (verbose) message('[ Parallel processing is disabled ]')
 
     clusters <- foreach::foreach(i = names(granges), .combine = rbind) %do% {
-      methRegions(beta, granges[[ i ]], verbose = verbose, ...) %>%
+      methRegions(beta, granges[[ i ]], verbose = verbose,
+                  thresholdDistance = thresholdDistance,
+                  rateFactor = rateFactor, scale = scale,
+                  minClusterSize = minClusterSize,
+                  compressionRatio = compressionRatio, seed = seed) %>%
         tibble::enframe(name = 'Name', 'Cluster') %>%
-        as.data.table %>%
+        data.table::as.data.table %>%
         .[ , Cluster := paste(i, Cluster, sep = '_') ]
     }
   }
 
   # Correct cluster names
-  clusters <- data.table(Old = clusters[ , unique(Cluster) ]) %>%
+  clusters <- data.table::data.table(Old = clusters[ , unique(Cluster) ]) %>%
     .[ , New := make.names(1:.N) ] %>%
     merge(clusters, by.x = 'Old', by.y = 'Cluster') %>%
-    setnames('New', 'ClusterID') %>%
+    data.table::setnames('New', 'ClusterID') %>%
     .[ , Old := NULL ] %>%
     .[ , list(Name, ClusterID) ]
 
